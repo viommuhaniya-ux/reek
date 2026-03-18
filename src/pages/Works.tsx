32aeaa9fc11cf4f45ced10d00c3f8b9fb5861aee
viommuhaniya-ux <viommuhaniya@gmail.com>
2026-03-18 20:37:31 +0530

    Reduce max width of Works component

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -251,7 +251,7 @@ export default function Works() {
               exit={{ scale: 0.95, opacity: 0 }}
               transition={{ duration: 0.3 }}
               onClick={(e) => e.stopPropagation()}
-              className="relative w-full max-w-6xl"
+              className="relative w-full max-w-3xl"
             >
               {/* Close button */}
               <button
